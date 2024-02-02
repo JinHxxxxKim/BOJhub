60d5f6415f27e36c107ae4e1808ae48244e40f62
JinHxxxxKim <83858427+JinHxxxxKim@users.noreply.github.com>
2024-02-02 13:37:22 +0900

    [Silver IV] Title: 카드2, Time: 124 ms, Memory: 23752 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,24 +1,41 @@
-/* package whatever; // don't place package name! */
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-public class Main
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-	    Queue<Integer> q = new LinkedList<>();
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		for(int i=0;i<N;i++)
-		    q.offer(i+1);
-        
-        while(q.size()!=1){
-            q.poll();
-            q.offer(q.poll());
-        }
-        System.out.println(q.poll());
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * @author 김진형
+ * 
+ * 1. N을 입력받는다.
+ * 2. 1~N까지 큐에 삽입한다.
+ * 3. 한번 뺴고, 재삽입을 반복한다. (size가 1이 될때까지)
+ */
+public class Main {
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static StringBuilder sb = new StringBuilder();
+	private static StringTokenizer st;
+
+	private static int N;
+	public static void main(String[] args) throws Exception {
+		N = Integer.parseInt(br.readLine().trim());
+		Queue<Integer> q = new ArrayDeque<Integer>();
+		for(int num=1;num<=N;++num) {
+			q.offer(num);
+		}
+		boolean isTrash = true;
+		while(q.size()>1) {
+			if(isTrash) {
+				q.poll();
+				isTrash = false;
+			}
+			else {
+				q.offer(q.poll());
+				isTrash = true;
+			}
+		}
+		System.out.println(q.poll());
 	}
 }
