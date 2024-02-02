e3e0987cb124be7eea7be3873be3294bb9232a31
JinHxxxxKim <83858427+JinHxxxxKim@users.noreply.github.com>
2024-02-02 14:01:56 +0900

    [Silver II] Title: 스택 수열, Time: 352 ms, Memory: 26552 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,84 +1,45 @@
-/* package whatever; // don't place package name! */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        Stack<Integer> stk = new Stack<>();
+        int cnt = 1;
+        for (int i = 0; i < N; ++i) {
+            int num = Integer.parseInt(br.readLine());
+            // 첫 숫자
 
-/* Name of the class has to be "Main" only if the class is public. */
-public class Main
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-	    int cnt = 0;
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
-		Integer[] arr = new Integer[N+1];
+            if (stk.isEmpty()) {
+                while (cnt <= num) {
+                    stk.push(cnt++);
+                    sb.append("+\n");
+                }
+            }
+            if (stk.peek() == num) {
+                stk.pop();
+                sb.append("-\n");
+            } else if (stk.peek() > num) {
+                System.out.println("NO");
+                return;
+            } else {
+                while (cnt <= num) {
+                    stk.push(cnt++);
+                    sb.append("+\n");
+                }
+                if (stk.peek() == num) {
+                    stk.pop();
+                    sb.append("-\n");
+                }
+            }
 
-		Stack<Integer> stk = new Stack<>();
-
-		for(int i=1;i<=N;i++)
-		    arr[i] = i;
-
-		int curIdx = 1;
-        boolean isItChecksHighestPoint = false;
-        
-		while(cnt<N){
-		    int num = Integer.parseInt(br.readLine());
-		    //System.out.println(num+"num");
-		    //System.out.println(curIdx);
-		    if(num<N && !isItChecksHighestPoint){
-		        //System.out.println("________");
-		        if(!stk.empty()&&num==stk.peek()){
-		            //System.out.println("aaaaaaaa");
-		            sb.append("-\n");
-		            stk.pop();
-		        }
-		        else if(stk.search(num)!=-1){
-		            System.out.println("NO");
-		            return;
-		        }
-		        else{
-		            while(num!=arr[curIdx]){
-		                //System.out.println(arr[curIdx]);
-		                stk.push(arr[curIdx]);
-		                if(curIdx>N){
-		                    System.out.println("NO");
-		                    return;
-		                }
-		                sb.append("+\n");
-		                curIdx++;
-		            }
-		            sb.append("+\n-\n");
-		            curIdx++;
-		        }
-		    }
-		    else if(num==N){
-		        while(num!=arr[curIdx]){
-		                //System.out.println(arr[curIdx]);
-		                stk.push(arr[curIdx]);
-		                if(curIdx>N){
-		                    System.out.println("NO");
-		                    return;
-		                }
-		                sb.append("+\n");
-		                curIdx++;
-		            }
-		            sb.append("+\n-\n");
-		            curIdx++;
-		        isItChecksHighestPoint = true;
-		    }
-		    else{
-		        if(num == stk.pop())
-		            sb.append("-\n");
-		        else{
-		            System.out.println("NO");
-		            return;
-		        }
-		            
-		    }
-		    cnt++;
-		}
-		System.out.println(sb.toString());
-	}
+        }
+        System.out.println(sb.toString());
+    }
 }
