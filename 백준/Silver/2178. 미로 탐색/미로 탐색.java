566b0cb376a44da9a83cf6cb3ff19ffdd5228177
JinHxxxxKim <83858427+JinHxxxxKim@users.noreply.github.com>
2024-02-07 17:40:35 +0900

    [Silver I] Title: 미로 탐색, Time: 88 ms, Memory: 12084 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,62 +1,78 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
+/**
+ * @author 김진형
+ * 
+ * 1. 필요한 입력을 모두 받는다.
+ * 2. (1, 1)에서 BFS를 실행한다.
+ * 3. 4방 탐색을 통해 다음 위치에 대한 검증을 한 뒤, 이동한다.
+ * 4. Queue<int[]> 에서 [0]: row, [1]: col, [2]: dist 값으로 설정한다
+ */
 public class Main {
-	
-	static int[][] map;
-	static int n;
-	static int m;
-	static boolean[][] visited;
-	static int[] dx = { -1, 1, 0, 0 }; //x방향배열-상하
-    	static int[] dy = { 0, 0, -1, 1 }; //y방향배열-좌우
+	private static final int[] dx = new int[] { -1, 1, 0, 0 };
+	private static final int[] dy = new int[] { 0, 0, 1, -1 };
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		
-		map = new int[n][m];
-		for(int i=0; i<n; i++) {
-			String s = br.readLine();
-			for(int j=0; j<m; j++) {
-				map[i][j] = s.charAt(j) - '0';
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static StringBuilder sb = new StringBuilder();
+	private static StringTokenizer st;
+
+	private static int N, M;
+	private static int[][] map;
+	private static boolean[][] visited;
+
+	public static void main(String[] args) throws Exception {
+		// 필요한 입력을 모두 받는다.
+		st = new StringTokenizer(br.readLine().trim());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		map = new int[N][M];
+		visited = new boolean[N][M];
+		for (int row = 0; row < N; ++row) {
+			String rowInfo = br.readLine().trim();
+			for (int col = 0; col < M; ++col) {
+				map[row][col] = rowInfo.charAt(col) - '0';
 			}
 		}
-		
-		visited = new boolean[n][m];
+
+		// BFS Start
+		Queue<int[]> q = new ArrayDeque<>();
+		int ans = 0;
+		// [행, 열, 거리]
+		q.offer(new int[] { 0, 0, 1 });
 		visited[0][0] = true;
-		bfs(0, 0);
-		System.out.println(map[n-1][m-1]);
-	}
+		while (!q.isEmpty()) {
+			int[] nodeInfo = q.poll();
+			int currRow = nodeInfo[0];
+			int currCol = nodeInfo[1];
+			int currDist = nodeInfo[2];
 
-	public static void bfs(int x, int y) {
-		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] {x,y});
-		
-		while(!q.isEmpty()) {
-			int now[] = q.poll();
-			int nowX = now[0];
-			int nowY = now[1];
-			
-			for(int i=0; i<4; i++) {
-				int nextX = nowX + dx[i];
-				int nextY = nowY + dy[i];
-				
-		                if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m)
-                		    continue;
-        		        if (visited[nextX][nextY] || map[nextX][nextY] == 0)
-                    		continue;
-                    
-		                q.add(new int[] {nextX, nextY});
-        		        map[nextX][nextY] = map[nowX][nowY] + 1;
-                		visited[nextX][nextY] = true;
+			// 목적지 도착
+			if (currRow == N - 1 && currCol == M - 1) {
+				ans = currDist;
+				break;
+			}
+
+			// 4방탐색
+			for (int dir = 0; dir < 4; ++dir) {
+				int tempRow = currRow + dx[dir];
+				int tempCol = currCol + dy[dir];
+
+				// 범위 검증
+				if (tempRow < 0 || tempCol < 0 || tempRow >= N || tempCol >= M || map[tempRow][tempCol] == 0)
+					continue;
+				// 방문 검증
+				if (visited[tempRow][tempCol])
+					continue;
+				visited[tempRow][tempCol] = true;
+				q.offer(new int[] { tempRow, tempCol, currDist + 1 });
 			}
 		}
+
+		System.out.println(ans);
 	}
 }
