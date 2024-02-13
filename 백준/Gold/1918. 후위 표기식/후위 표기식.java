cbd04aedbc01823600af798b8a9587bb6a1364ac
JinHxxxxKim <83858427+JinHxxxxKim@users.noreply.github.com>
2024-02-13 17:48:21 +0900

    [Gold II] Title: 후위 표기식, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.java
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
@@ -11,11 +12,18 @@ import java.util.StringTokenizer;
  * 
  * 1. 중위 표기식을 입력받는다.
  * 2. 중위 표기식의 0번째 인덱스부터 순회한다.
- * 3. 숫자의 경우, 그냥 append
- * 4. 연산자의 경우
- * 		- 스택이 비어있다면 push
- * 		- 비어있지 않다면, 연산자 비교 후, 나보다 큰 연산자의 경우 pop 후 push
- * 		- 괄호의 경우, 여는 괄호는 push, 닫는 괄호까지 위의 과정 반복
+ * 
+ * 현재 문자가 가능한 경우
+ * 		1. 피연산자인 경우
+ * 			- StringBuilder에 그냥 append한다.
+ * 		2. 연산자인 경우
+ * 			- stack이 비어있거나, 가장 최근(peek)연산자가 '('일 경우 그냥 push한다.
+ * 			- '+', '-' 인 경우: '('가 나오거나, 스택이 비지 않을 때 까지 pop한 뒤 append한다. 
+ * 			- '*', '/' 인 경우: '('가 나오거나, 스택이 비지 않을 때 까지 + 연산자의 순서를 고려하여 '*', '/'이 나오지 않을 때까지 pop한 뒤 append한다.  
+ * 		3. 여는 괄호인 경우
+ * 			- StringBuilder에 그냥 append한다.
+ * 		4. 닫는 괄호인 경우
+ * 			- 여는 괄호가 나올 때까지 stack에서 pop한 뒤 append한다.
  */
 public class Main {
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,12 +31,18 @@ public class Main {
 	private static StringTokenizer st;
 
 	public static void main(String[] args) throws Exception {
+		// 중위 연산식 입력
 		String infixString = br.readLine().trim();
 		char[] charInfixArray = infixString.toCharArray();
 		Stack<Character> stack = new Stack<>();
+		
+		// 0번 인덱스 문자부터 끝까지 순회 
 		for (int idx = 0; idx < charInfixArray.length; ++idx) {
-			char currChar = charInfixArray[idx];	
-			if (currChar == '+' || currChar == '-') {
+			// 현재 문자
+			char currChar = charInfixArray[idx];
+			switch (currChar) {
+			case '+':
+			case '-':
 				if (stack.isEmpty() || stack.peek() == '(') {
 					stack.push(currChar);
 				}else {
@@ -37,7 +51,9 @@ public class Main {
 					}
 					stack.push(currChar);
 				}
-			} else if (currChar == '*' || currChar == '/') {
+				break;
+			case '*':
+			case '/':
 				if (stack.isEmpty() || stack.peek() == '(') {
 					stack.push(currChar);
 				}else {
@@ -46,15 +62,19 @@ public class Main {
 					}
 					stack.push(currChar);
 				}
-			} else if (currChar == '(') {
+				break;
+			case '(':
 				stack.push(currChar);
-			} else if (currChar == ')') {
+				break;
+			case ')':
 				while(stack.peek()!='(')
 					sb.append(stack.pop());
 				if(stack.peek()=='(')
 					stack.pop();
-			} else {
+				break;
+			default:
 				sb.append(currChar);
+				break;
 			}
 		}
 		while(!stack.isEmpty())
