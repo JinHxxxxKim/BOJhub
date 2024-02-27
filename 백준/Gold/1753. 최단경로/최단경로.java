c93ae72525ec9b9942b505c285662392d0131b35
JinHxxxxKim <83858427+JinHxxxxKim@users.noreply.github.com>
2024-02-27 14:27:10 +0900

    [Gold IV] Title: 최단경로, Time: 788 ms, Memory: 115648 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,107 +1,125 @@
+
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
+/**
+ * @author 김진형
+ * 
+ * 다익스트라 알고리즘
+ * 
+ * 1. dist[] 배열을 INF로 모두 초기화한다.
+ * 2. 시작 정점의 dist값을 0으로 갱신하고, 우선순위 큐에 넣는다
+ * 3. 우선순위 큐에서 꺼내서 인접 정점까지의 가중치를 확인하고 
+ *    현재 자신의 dist값에 더하고 인접 정점의 dist를 갱신하고 우선순위 큐에 넣는다.
+ * 4. 인접 정점 확인 시, 이미 방문한 정점은 PASS
+ * 5. 모든 정점을 확인하였다면 break
+ */
 public class Main {
-    private static int V;
-    private static int E;
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static StringBuilder sb = new StringBuilder();
+	private static StringTokenizer st;
 
-    private static boolean[] visited;
-    private static int[] dist;
+	private static final int INF = Integer.MAX_VALUE;
+	
+	static int V, E;
+	static List<Node>[] adList;
+	static boolean[] isVisited;
+	static int[] dist;
+	static int startNode;
+	
+	public static void main(String[] args) throws Exception {
+		st = new StringTokenizer(br.readLine().trim());
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		startNode = Integer.parseInt(br.readLine().trim());
 
-    private static ArrayList<Edge1753>[] adList;
+		adList = new List[V + 1];
+		isVisited = new boolean[V + 1];
+		dist = new int[V + 1];
+		Arrays.fill(dist, INF);
+		dist[startNode] = 0;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-        int startNodeNum = Integer.parseInt(br.readLine());
-        adList = new ArrayList[V + 1];
-        for (int i = 1; i <= V; ++i) {
-            adList[i] = new ArrayList<Edge1753>();
-        }
-        for (int i = 0; i < E; ++i) {
-            st = new StringTokenizer(br.readLine());
-            int srcNode = Integer.parseInt(st.nextToken());
-            int destNode = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            adList[srcNode].add(new Edge1753(destNode, weight));
-        }
-        visited = new boolean[V + 1];
-        dist = new int[V + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        PriorityQueue<Node1753> pq = new PriorityQueue<>();
-        pq.offer(new Node1753(startNodeNum, 0));
-        dist[startNodeNum] = 0;
-        while (!pq.isEmpty()) {
-            Node1753 currNode = pq.poll();
-            int currNodeNum = currNode.getNodeNum();
-            int currWeight = currNode.getWeight();
-            if (visited[currNodeNum]) {
-                continue;
-            }
-            visited[currNodeNum] = true;
-            for (Edge1753 edge : adList[currNodeNum]) {
-                int destNodeNum = edge.getDestNode();
-                int useCost = edge.getWeight();
-                if (visited[destNodeNum]) {
-                    continue;
-                }
-                dist[destNodeNum] = Math.min(dist[destNodeNum], currWeight + useCost);
-                pq.offer(new Node1753(destNodeNum, dist[destNodeNum]));
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= V; ++i) {
-            if (dist[i] == Integer.MAX_VALUE) {
-                sb.append("INF");
-            } else {
-                sb.append(dist[i]);
-            }
-            sb.append("\n");
-        }
-        System.out.println(sb);
-    }
-}
+		// 인접 리스트 생성
+		for (int idx = 1; idx <= V; ++idx)
+			adList[idx] = new ArrayList<>();
+		for (int edgeCnt = 0; edgeCnt < E; ++edgeCnt) {
+			st = new StringTokenizer(br.readLine().trim());
+			int srcNode = Integer.parseInt(st.nextToken());
+			int destNode = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
 
-class Node1753 implements Comparable<Node1753> {
-    private int nodeNum;
-    private int weight;
+			adList[srcNode].add(new Node(destNode, weight));
+		}
 
-    public Node1753(int nodeNum, int weight) {
-        this.nodeNum = nodeNum;
-        this.weight = weight;
-    }
+		for (int idx = 1; idx <= V; ++idx) {
+			Collections.sort(adList[idx]);
+		}
+//		for (int idx = 1; idx <= V; ++idx) {
+//			System.out.println(adList[idx]);
+//		}
+		// 다익스트라 알고리즘 시작
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(startNode, 0));
+		int visitCnt = 0;
+		while (!pq.isEmpty()) {
+			Node currNode = pq.poll();
+			int currNodeNum = currNode.nodeNum;
+			int currCost = currNode.weight;
+			if(isVisited[currNodeNum])
+				continue;
+			isVisited[currNodeNum] = true;
+//			System.out.println(Arrays.toString(dist));
+//			System.out.println("currNodeNum:" + currNodeNum);
+//			System.out.println("currCost: " + currCost);
+			
+			visitCnt++;
+			if (visitCnt == V) {
+				break;
+			}
+			// 인접 노드 탐색
+			for (Node adNode : adList[currNodeNum]) {
+				if (!isVisited[adNode.nodeNum] && currCost + adNode.weight < dist[adNode.nodeNum]) {
+//					System.out.println("BBB");
+					dist[adNode.nodeNum] = currCost + adNode.weight;
+					pq.offer(new Node(adNode.nodeNum, dist[adNode.nodeNum]));
+				}
+			}
+		}
+		for (int idx = 1; idx <= V; ++idx) {
+			if (dist[idx] == Integer.MAX_VALUE)
+				sb.append("INF").append("\n");
+			else
+				sb.append(dist[idx]).append("\n");
+		}
 
-    public int getNodeNum() {
-        return nodeNum;
-    }
+		System.out.println(sb);
+	}
 
-    public int getWeight() {
-        return weight;
-    }
+	// 정점 클래스
+	static class Node implements Comparable<Node> {
+		int nodeNum, weight;
 
-    @Override
-    public int compareTo(Node1753 node1753) {
-        return this.weight - node1753.getWeight();
-    }
-}
+		public Node(int nodeNum, int weight) {
+			this.nodeNum = nodeNum;
+			this.weight = weight;
+		}
 
-class Edge1753 {
-    private int destNode;
-    private int weight;
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(this.weight, o.weight);
+		}
 
-    public Edge1753(int destNode, int weight) {
-        this.destNode = destNode;
-        this.weight = weight;
-    }
-
-    public int getDestNode() {
-        return destNode;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
+		@Override
+		public String toString() {
+			return "Node [nodeNum=" + nodeNum + ", weight=" + weight + "]";
+		}
+		
+	}
 }
