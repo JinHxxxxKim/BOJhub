a3291b711d8e3d07dc0acb0755f5c926f22c1a2a
JinHxxxxKim <83858427+JinHxxxxKim@users.noreply.github.com>
2024-02-28 15:08:38 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 76 ms, Memory: 11808 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -2,11 +2,12 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
-
+ * @author 
  * 
  * 1. 첫 파이프의 끝점을 시작점으로 BFS 탐색
  * 		- 파이르가 놓인 방향을 함꼐 저장한다
@@ -24,7 +25,8 @@ public class Main {
 
 	static int N;
 	static int[][] map;
-
+	static int[][][] DP;
+	
 	public static void main(String[] args) throws Exception {
 		N = Integer.parseInt(br.readLine().trim());
 		map = new int[N][N];
@@ -34,7 +36,56 @@ public class Main {
 				map[row][col] = Integer.parseInt(st.nextToken());
 			}
 		}
+		
+//		solution1();
+		
+		// 수평, 수직, 대각선
+		DP = new int[3][N][N];
 
+		// DP초기화
+		for (int col = 1; col < N; ++col) {
+			if (map[0][col] == 1)
+				break;
+			DP[HORIZONTAL][0][col] = 1;
+		}
+		
+		
+		for (int row = 1; row < N; ++row) {
+			for (int col = 2; col < N; ++col) {
+				if(map[row][col] == 1)
+					continue;
+				
+				// 해당 위치를 끝점으로 가로로 놓이는 경우
+				DP[HORIZONTAL][row][col] = DP[HORIZONTAL][row][col - 1] + DP[DIAGONAL][row][col - 1];
+				
+				// 해당 위치를 끝점으로 세로로 놓이는 경우
+				DP[VERTICAL][row][col] = DP[VERTICAL][row - 1][col] + DP[DIAGONAL][row - 1][col];
+
+				// 해당 위치를 끝점으로 대각선으로 놓이는 경우
+				if (map[row - 1][col] != 1 && map[row][col - 1] != 1) {
+					DP[DIAGONAL][row][col] = DP[HORIZONTAL][row - 1][col - 1] 
+											+ DP[VERTICAL][row - 1][col - 1] 
+											+ DP[DIAGONAL][row - 1][col - 1];
+				}
+			}
+		}
+		
+//		for (int[][] h : DP) {
+//			for (int[] r : h) {
+//				System.out.println(Arrays.toString(r));
+//			}
+//			System.out.println("+================+");
+//		}
+		
+		int sum = 0;
+		for (int idx = 0; idx < 3; ++idx) {
+			sum += DP[idx][N - 1][N - 1];
+		}
+		System.out.println(sum);
+		
+	}
+	
+	private static void solution1() {
 		int ans = 0;
 		Queue<Pipe> q = new ArrayDeque<>();
 		q.offer(new Pipe(0, 1, HORIZONTAL));
@@ -88,8 +139,9 @@ public class Main {
 		}
 		
 		System.out.println(ans);
+		
 	}
-	
+
 	static class Pipe{
 		int x, y;
 		int dir;
