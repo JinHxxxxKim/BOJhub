4c3fceedb49a347f80f770f936b939915902e699
JinHxxxxKim <83858427+JinHxxxxKim@users.noreply.github.com>
2024-03-05 18:41:19 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 114 ms, Memory: 19,860 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,80 +1,80 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * @author 김진형
+ * 
+ * 1. 상자의 높이를 모두 입력받는다.
+ * 2. 덤프 횟수만큼 반복한다.
+ * 	2-1. 최고점과 최저점을 찾은 뒤, 조정해준다.
+ * 	2-2. 최고점과 최저점의 높이가 동일하거나 1차이가 난다면  종료한다.
+ * 3. 최고점과 최저점의 높이의 차이를 반환한다.
+ */
 public class Solution {
-    private static int[] height;
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static StringBuilder sb = new StringBuilder();
+	private static StringTokenizer st;
 
-    public static void main(String[] args) throws Exception {
-        StringBuilder sb = new StringBuilder();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for (int test_case = 1; test_case <= 10; test_case++) {
-            int ans = solution(br);
-            sb.append("#");
-            sb.append(test_case);
-            sb.append(" ");
-            sb.append(ans);
-            sb.append("\n");
-        }
-        System.out.println(sb);
-    }
+	static int N;
+	static int[] heights;
+	static int dumpCnt; // 덤프 횟수
+	
+	public static void main(String[] args) throws Exception {
+		int TC = 10;
+		for (int testCase = 1; testCase <= TC; ++testCase) {
+			dumpCnt = Integer.parseInt(br.readLine().trim());
+			heights = new int[100];
+			
+			// 높이 입력받기
+			st = new StringTokenizer(br.readLine().trim());
+			for(int idx = 0; idx<100;++idx) {
+				heights[idx] = Integer.parseInt(st.nextToken());
+			}
+			
+			// 덤프횟수만큼 반복
+			while (dumpCnt > 0) {
+				int minHeight = 101, maxHeight = 0; // 최대, 최소 높이
+				int minHeightIdx = -1, maxHeightIdx = -1; // 최대 최소 높이를 갖는 인덱스
+				// 탐색
+				for (int idx = 0; idx < 100; ++idx) {
+					if (minHeight > heights[idx]) {
+						minHeight = heights[idx];
+						minHeightIdx = idx;
+					}
 
-    private static int solution(BufferedReader br) throws Exception{
-        height = new int[100];
-        int dumpCount = Integer.parseInt(br.readLine());
-        System.out.println(dumpCount);
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 100; ++i) {
-            height[i] = Integer.parseInt(st.nextToken());
-        }
-        while (dumpCount > 0) {
-            dump();
-            --dumpCount;
-        }
-        return getDiff();
-    }
+					if (maxHeight < heights[idx]) {
+						maxHeight = heights[idx];
+						maxHeightIdx = idx;
+					}
+				}
 
-    private static int getDiff() {
-        int maxHeight = Integer.MIN_VALUE;
-        int minHeight = Integer.MAX_VALUE;
+				// 종료조건
+				if (Math.abs(maxHeight - minHeight) <= 1) {
+					break;
+				}
+				
+				// 덤프 진행
+				heights[minHeightIdx]++;
+				heights[maxHeightIdx]--;
+				--dumpCnt;
+			}
+			
+			int minHeight = 101, maxHeight = 0; // 최대, 최소 높이
+			// 탐색
+			for (int idx = 0; idx < 100; ++idx) {
+				if (minHeight > heights[idx]) {
+					minHeight = heights[idx];
+				}
 
-        for (int i = 0; i < 100; ++i) {
-            if (maxHeight < height[i]) {
-                maxHeight = height[i];
-            }
-        }
-
-        for (int i = 0; i < 100; ++i) {
-            if (minHeight > height[i]) {
-                minHeight = height[i];
-            }
-        }
-        return maxHeight - minHeight;
-    }
-
-    private static void dump() {
-        int maxHeight = Integer.MIN_VALUE;
-        int maxHeightIdx = -1;
-
-        int minHeight = Integer.MAX_VALUE;
-        int minHeightIdx = -1;
-
-        for (int i = 0; i < 100; ++i) {
-            if (maxHeight < height[i]) {
-                maxHeight = height[i];
-                maxHeightIdx = i;
-            }
-        }
-
-        for (int i = 0; i < 100; ++i) {
-            if (minHeight > height[i]) {
-                minHeight = height[i];
-                minHeightIdx = i;
-            }
-        }
-
-        --height[maxHeightIdx];
-        ++height[minHeightIdx];
-    }
+				if (maxHeight < heights[idx]) {
+					maxHeight = heights[idx];
+				}
+			}
+			sb.append(String.format("#%d %d\n", testCase, Math.abs(maxHeight - minHeight)));
+		}
+		System.out.println(sb);
+	}
 }
+
