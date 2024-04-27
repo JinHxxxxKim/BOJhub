ead381931c46f67486f12a89b8b5dc5f9ec2f5e5
JinHxxxxKim <83858427+JinHxxxxKim@users.noreply.github.com>
2024-04-28 00:29:00 +0900

    [Silver IV] Title: ATM, Time: 84 ms, Memory: 11836 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * @author JinHyung
+ * 
+ * 누적합
+ * 
+ * 1. 인출 시간을 모두 입력받는다.
+ * 2. 순서가 빠를 수록 더해지는 횟수가 많다 => 앞 순위로 배치한다.
+ *   - 증 오름차순으로 정렬한다.
+ * 3. 각 소요시가느이 누적합을 구한다.
+ * 4. 모든 누적합을 더한다.
+ *
+ */
+public class Main {
+	static final StringBuilder sb = new StringBuilder();
+	static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	
+	static int N;
+	static int[] reqTime;
+	static int[] cuSum;
+
+	public static void main(String[] args) throws Exception {
+		N = Integer.parseInt(br.readLine().trim());
+		reqTime = new int[N];
+		cuSum = new int[N + 1];
+		st = new StringTokenizer(br.readLine().trim());
+		
+		for (int idx = 0; idx < N; ++idx) 
+			reqTime[idx] = Integer.parseInt(st.nextToken());
+		
+		int ans = 0;
+		Arrays.sort(reqTime);
+		for (int idx = 1; idx < N + 1; ++idx) {
+			cuSum[idx] = cuSum[idx - 1] + reqTime[idx - 1];
+			ans += cuSum[idx];
+		}
+		System.out.println(ans);
+	}
+}
