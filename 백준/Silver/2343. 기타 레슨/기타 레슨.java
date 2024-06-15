33d1ec580f9df2a9d71f475136a987c1645f0cdf
JinHxxxxKim <83858427+JinHxxxxKim@users.noreply.github.com>
2024-06-15 19:36:06 +0900

    [Silver I] Title: 기타 레슨, Time: 236 ms, Memory: 22416 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -1,58 +1,79 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * [기타레슨] - BOJ
+ * 이진탐색
+ *  N (1 ≤ N ≤ 100,000)과 M (1 ≤ M ≤ N)
+ *  각 강의 시간은 10000분 이하
+ *
+ * 블루레이 크기의 "최솟값"을 찾는 것이 목표
+ * => 블루레이 크기: 이진탐색의 대상
+ * => 초기 left 값: 1, right 값: 강의 길이의 합
+ *
+ */
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int N, M;
+    static int[] records;
+
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] array = new int[N];
-        st = new StringTokenizer(br.readLine());
-        int lowerBound = Integer.MIN_VALUE;
-        int upperBound = 0;
-        for (int i = 0; i < N; ++i) {
-            array[i] = Integer.parseInt(st.nextToken());
-            upperBound += array[i];
-            lowerBound = Math.max(lowerBound, array[i]);
+        st = new StringTokenizer(br.readLine().trim());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        records = new int[N];
+
+        long left = 1L; // 초기 블루레이 하한값
+        long right = 0L; // 초기 블루레이 상한값
+        
+        // 레코드 길이 입력
+        st = new StringTokenizer(br.readLine().trim());
+        for(int idx = 0; idx < N; idx++) {
+            records[idx] = Integer.parseInt(st.nextToken());
+            right += records[idx];
+            left = Math.max(left, records[idx]);
         }
-        //System.out.println(Arrays.toString(array));
 
-        int ans = Integer.MAX_VALUE;
+        long ans = Long.MAX_VALUE;
+        // 이진탐색 시작
+        while (left <= right) {
+            long mid = (left + right) / 2; // 확인하고자 하는 블루레이 크기
+            // 해당 블루레이 크기에 모든 강의가 담길 수 있는지 확인
+            int reqDisk = getDisckCount(mid);
 
-        while (lowerBound <= upperBound) {
-            int mid = (lowerBound + upperBound) / 2; // 블루레이 크기
-//            System.out.println("=================");
-//            System.out.println("lowerBound = " + lowerBound);
-//            System.out.println("mid = " + mid);
-//            System.out.println("upperBound = " + upperBound);
-            int sum = 0;
-            int blueRayCnt =0;
-
-            for (int i = 0; i < N; ++i) {
-                if (array[i] + sum > mid) {
-                    sum = 0;
-                    blueRayCnt++;
-                }
-                sum += array[i];
-            }
-            if (sum != 0) {
-                blueRayCnt++;
-            }
-
-//            System.out.println("++++++++++++++++++++");
-//            System.out.println("blueRayCnt = " + blueRayCnt);
-            if (blueRayCnt > M) {
-                lowerBound = mid + 1;
-                continue;
+            if (reqDisk > M) {
+                // 필요 블루레이 개수보다 많이 사용한다 -> 블루레이 크기를 늘릴필요가 있다.
+                left = mid + 1;
             } else {
-                upperBound = mid - 1;
+                // 필요 블루레이 개수보다 적거나 같게 사용한다 -> 블루레이 크기를 줄일 필요가 있다.
+                right = mid - 1;
+                ans = Math.min(ans, mid);
             }
-            ans = Math.min(ans, mid);
 
         }
         System.out.println(ans);
     }
+
+    // 해당 disk 사이즈에 대해 요구되는 블루레이 개수 반환
+    private static int getDisckCount(long diskSize) {
+        int diskCnt = 0;
+        long currDiskStatus = 0; // 현재 블루레이에 대해 사용한 용량(size) 카운팅
+
+        for (int record : records) {
+            if(record + currDiskStatus > diskSize) {
+                currDiskStatus = 0;
+                ++diskCnt;
+            }
+            currDiskStatus += record;
+        }
+        if (currDiskStatus != 0) {
+            ++diskCnt;
+        }
+        return diskCnt;
+    }
+    
 }
