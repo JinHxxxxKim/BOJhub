a1787ef5e95799155ca5eee33317c509c63fcffe
JinHxxxxKim <83858427+JinHxxxxKim@users.noreply.github.com>
2024-06-18 15:12:52 +0900

    [Silver IV] Title: 동전 0, Time: 68 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,31 +1,37 @@
-/* package whatever; // don't place package name! */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+/**
+ * [동전0] - BOJ
+ * 그리디
+ * 1.
+ */
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Main
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		int N, K;
-		int numOfCoin = 0;
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		int[] typeOfCoin = new int[N];
-		
-		for(int i=0;i<N;i++)
-			typeOfCoin[N-1-i] = Integer.parseInt(bf.readLine());
-		
-		for(int i=0;i<N;i++){
-			if(typeOfCoin[i]<=K){
-				numOfCoin += (K/typeOfCoin[i]);
-				K = K%typeOfCoin[i];
-			}
-		}
-		System.out.println(numOfCoin);
-	}
+    static int N, K;
+    static int[] coinSize;
+    static int coinCnt;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine().trim());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        coinSize = new int[N];
+        for (int idx = 0; idx < N; ++idx) {
+            coinSize[idx] = Integer.parseInt(br.readLine().trim());
+        }
+
+        coinCnt = 0;
+
+        for (int idx = N - 1; idx >= 0; --idx) {
+            int currCoin = K / coinSize[idx];
+            coinCnt += currCoin;
+            K -= currCoin * coinSize[idx];
+        }
+
+        System.out.println(coinCnt);
+    }
 }
