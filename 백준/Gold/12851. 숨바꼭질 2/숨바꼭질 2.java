e5d71faea4f213ca48f8a88c2724d17602990c7f
JinHxxxxKim <83858427+JinHxxxxKim@users.noreply.github.com>
2025-02-05 16:09:47 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 156 ms, Memory: 26172 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int src, dest;
+    static int ans = 0;
+    static int minCnt = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine().trim());
+        src = Integer.parseInt(st.nextToken());
+        dest = Integer.parseInt(st.nextToken());
+
+        int[] dist = new int[100001];
+        boolean[] visited = new boolean[100001];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(src);
+        visited[src] = true;
+        dist[src] = 0;
+        while (!q.isEmpty()) {
+            int currNum = q.poll();
+            if (currNum == dest) {
+                ++ans;
+            }
+            // 범위 check
+            if (currNum + 1 < 100001 && dist[currNum + 1] > dist[currNum]) {
+                // 목적지 도착
+//                if (currNum + 1 == dest) {
+//                    ++ans;
+//                }
+                dist[currNum + 1] = dist[currNum] + 1;
+                q.offer(currNum + 1);
+            }
+
+            if (currNum - 1 >= 0 && dist[currNum - 1] > dist[currNum]) {
+                // 목적지 도착
+//                if (currNum - 1 == dest) {
+//                    ++ans;
+//                }
+                dist[currNum - 1] = dist[currNum] + 1;
+                q.offer(currNum - 1);
+            }
+
+            if (currNum * 2 < 100001 && dist[currNum * 2] > dist[currNum]) {
+                // 목적지 도착
+//                if (currNum * 2 == dest) {
+//                    ++ans;
+//                }
+                dist[currNum * 2] = dist[currNum] + 1;
+                q.offer(currNum * 2);
+            }
+
+        }
+        System.out.println(dist[dest]);
+        System.out.println(ans);
+    }
+}
