5d85097c5c9c40ba9c4480f7926285901cb5aef8
JinHxxxxKim <83858427+JinHxxxxKim@users.noreply.github.com>
2025-02-15 21:49:31 +0900

    [Gold II] Title: 문제집, Time: 396 ms, Memory: 48256 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -9,8 +9,6 @@ public class Main {
     static int V, E;
     static List<Integer>[] adList;
     static PriorityQueue<Integer> pq;
-    static boolean[] isSolved;
-    static boolean[] hasPreSolved;
     static int[] preSolvedCnt;
 
     public static void main(String[] args) throws Exception {
@@ -19,8 +17,6 @@ public class Main {
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
         adList = new ArrayList[V + 1];
-        isSolved = new boolean[V + 1];
-        hasPreSolved = new boolean[V + 1];
         preSolvedCnt = new int[V + 1];
         for (int idx = 1; idx <= V; ++idx) {
             adList[idx] = new ArrayList<>();
@@ -30,31 +26,24 @@ public class Main {
             int node1 = Integer.parseInt(st.nextToken());
             int node2 = Integer.parseInt(st.nextToken());
             adList[node1].add(node2);
-            hasPreSolved[node2] = true;
             preSolvedCnt[node2]++;
         }
 
         // SOLUTION
         pq = new PriorityQueue<>();
         for (int node = 1; node <= V; ++node) {
-            if (!hasPreSolved[node]) {
+            if (preSolvedCnt[node] == 0) {
                 pq.offer(node);
             }
         }
 
         while (!pq.isEmpty()) {
             int currProb = pq.poll();
-            if (isSolved[currProb]) {
-                continue;
-            }
             sb.append(currProb).append(" ");
-            isSolved[currProb] = true;
             for (int nextProb : adList[currProb]) {
                 --preSolvedCnt[nextProb];
-                if (!pq.contains(nextProb) && !isSolved[nextProb]) {
-                    if (preSolvedCnt[nextProb] == 0) {
-                        pq.offer(nextProb);
-                    }
+                if (preSolvedCnt[nextProb] == 0) {
+                    pq.offer(nextProb);
                 }
             }
         }
