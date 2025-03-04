845ab7718b968e7f3f929b57f235ee1e82a30adf
JinHxxxxKim <83858427+JinHxxxxKim@users.noreply.github.com>
2025-03-04 18:40:09 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -0,0 +1,36 @@
+-- 코드를 입력하세요
+SELECT 
+    H.HISTORY_ID
+    , CASE
+        WHEN 
+            (DATEDIFF(H.END_DATE, H.START_DATE) + 1) >= 90 
+        THEN
+            ROUND(((DATEDIFF(H.END_DATE, H.START_DATE) + 1) * C.DAILY_FEE)  
+            * 
+            ((100 - (SELECT DISCOUNT_RATE FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN 
+             WHERE CAR_TYPE = '트럭' AND DURATION_TYPE = '90일 이상')) / 100), 0)
+        WHEN 
+            (DATEDIFF(H.END_DATE, H.START_DATE) + 1) >= 30 
+        THEN
+            ROUND(((DATEDIFF(H.END_DATE, H.START_DATE) + 1) * C.DAILY_FEE)  
+            * 
+            ((100 - (SELECT DISCOUNT_RATE FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN 
+             WHERE CAR_TYPE = '트럭' AND DURATION_TYPE = '30일 이상')) / 100), 0)
+        WHEN 
+            (DATEDIFF(H.END_DATE, H.START_DATE) + 1) >= 7 
+        THEN
+            ROUND(((DATEDIFF(H.END_DATE, H.START_DATE) + 1) * C.DAILY_FEE)  
+            * 
+            ((100 - (SELECT DISCOUNT_RATE FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN 
+             WHERE CAR_TYPE = '트럭' AND DURATION_TYPE = '7일 이상')) / 100), 0)
+        ELSE 
+            ROUND(((DATEDIFF(H.END_DATE, H.START_DATE) + 1) * C.DAILY_FEE), 0)
+        END AS FEE
+FROM 
+    CAR_RENTAL_COMPANY_CAR AS C 
+    INNER JOIN 
+    CAR_RENTAL_COMPANY_RENTAL_HISTORY AS H 
+    ON C.CAR_ID = H.CAR_ID
+WHERE 
+    C.CAR_TYPE = '트럭'
+ORDER BY FEE DESC, HISTORY_ID DESC;
